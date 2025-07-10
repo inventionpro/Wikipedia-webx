9201c6579b9299086a9d858884193632b10dd6a8
inventionpro <109528211+inventionpro@users.noreply.github.com>
2025-07-10 16:13:56 +0200

    Cleaner experience

--- script.lua
+++ script.lua
@@ -1,4 +1,4 @@
-get('search').on_click(function()
+function search()
     local extra = '&lang=' .. get('lang').get_contents();
     if window ~= nil then
         if window.browser == 'bussinga' then
@@ -12,6 +12,16 @@ get('search').on_click(function()
         body = ''
     })
 
+    local data = res.data:gsub(">%s+<", "><");
+    data = data:gsub('class="link"%s+href="(.-)"', 'onclick="event.preventDefault();document.querySelector(`query`).value=`%1`.split(`/`).slice(-1)[0];document.querySelector(`search`).click()"')
+
     get('titl').set_contents(res.title)
-    get('txt').set_contents(res.data:gsub(">%s+<", "><"))
+    get('txt').set_contents(data)
+end
+
+get('search').on_click(function()
+    search()
+end)
+get('query').on_submit(function()
+    search()
 end)
